798539df629763830cd3e4b53a2dd67a50cd0535
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-09-09 18:37:51 -0300

    Criando o projeto do Jogo da Velha

---
+++ src/main/java/br/com/philipe/Main.java
@@ -0,0 +1,9 @@
+package br.com.philipe;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Tic tac toe");
+	}
+
+}
---
+++ src/main/java/br/com/philipe/core/Game.java
@@ -0,0 +1,5 @@
+package br.com.philipe.core;
+
+public class Game {
+
+}
