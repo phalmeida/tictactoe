cc54501a5ac30929e4a602dccc0eaae9dc0619c4
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-09-16 19:06:23 -0300

    Detectando o próximo jogador a jogar

--- src/main/java/br/com/philipe/tictactoe/core/Game.java
+++ src/main/java/br/com/philipe/tictactoe/core/Game.java
@@ -7,6 +7,7 @@ public class Game {
 	
 	private Board board = new Board();
 	private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
+	private int currentPlayerIndex = 0;
 	
 	public void play() {
 		UI.printGameTitle();
@@ -17,13 +18,22 @@ public class Game {
 	
 	}
 	
-	
 	private Player createPlayer(int index) {
 		String name = UI.readInput("Jogador " + (index + 1) + " =>");
 		char symbol = Constants.SYMBOL_PLAYERS[index];
 		Player player = new Player(name, board, symbol);
 		UI.printText(" O jogador '" + name + "' vai utilizar o símbolo '" + symbol +"'");
 		return player;
+	}
+	
+	private Player nextPlayer() {
+		currentPlayerIndex++;
+		
+		if(currentPlayerIndex >= players.length) {
+			currentPlayerIndex = 0;
+		}
+		
+		return players[currentPlayerIndex]
 	}
 
 }
